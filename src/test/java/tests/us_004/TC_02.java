d06a41f9c7b49a12f581dd8be55664b863bc773a
Oguzhan Yucedal <yucedaloguzhan@gmail.com>
2022-02-18 12:42:20 +0300

    oguz

--- src/test/java/tests/us_004/TC_02.java
+++ src/test/java/tests/us_004/TC_02.java
@@ -18,35 +18,54 @@ public class TC_02 extends TestBaseRapor {
     //TC_002 Code,Name,Adress,Phone,Email bilgileri ilgili alana girilebilmeli
     @Test
     public void test02(){
-        extentTest=extentReports.createTest("kullanıcı hesabı olusturma islemi testi",
-                "kullanıcı hesap olusturma islemlerini yapmasi gerekir");
+        extentTest=extentReports.createTest("Ilgili alanlari doldurma testi",
+                "yonetici ilgili kutucuklari gorup doldurmali");
+
         //1- Yonetici url e gider.
         Driver.getDriver().get(ConfigReader.getProperty("HMCUrl"));
-        extentTest.info("Url girildi");
+        extentTest.info("Url gidildi");
+
         //2- Login butonunu tiklar.
         hmcPages.firstLogInElement.click();
+        extentTest.info("Log in butonuna tiklandi");
+
         //3- Yonetici kendisine verilen username ve password ile giris yapar.
         hmcPages.userNameBoxElement.sendKeys(ConfigReader.getProperty("HMCValidUsername"));
         hmcPages.passwordBoxElement.sendKeys(ConfigReader.getProperty("HMCValidPassword"));
         hmcPages.secondLogInElement.click();
+        extentTest.info("Gecerli username ve password ile giris yapildi");
+
         //4- "Hotel management" kismina tiklar.
         hmcPages.hotelManagementLink.click();
+        extentTest.info("Hotel management linkine tiklandi");
+
         //5- Gorunur hale gelen "Hotel List" kismina tiklar.
         hmcPages.hotelListLinki.click();
-        //7- "Add Hotel" butonuna erisebilir.
+        extentTest.info("Hotel List linkine tiklandi");
+
+        //6- "Add Hotel" butonuna erisebilir.
         Assert.assertTrue(hmcPages.addHotelLinki.isDisplayed());
-        hmcPages.addHotelLinki.click();
+        extentTest.info("Add hotel kisminin gorunur oldugu goruldu");
+
         //7-"Code" kutucuğuna tıklayıp bir Kod giriniz
         hmcPages.addHotelCodeBox.sendKeys("187150015");
+        extentTest.info("Code kutucuguna veri gonderildi");
 
         //8-"Name" kutucuğuna tıklayıp bir İsim giriniz
         us_004.createHotelNameBox.sendKeys("Oguzhan");
+        extentTest.info("Name kutucuguna veri gonderildi");
+
         //9-"Adress" kutucuğuna tıklayıp Adres giriniz
         us_004.createHotelAddressBox.sendKeys("13313 cutten rd #3201");
+        extentTest.info("Adress kutucuguna veri gonderildi");
+
         //10-"Phone" kutucuğuna tıklayıp Telefon Numarası giriniz
         us_004.createHotelPhoneBox.sendKeys("8325604012");
+        extentTest.info("Phone kutucuguna veri gonderildi");
+
         //11-"Mail" kutucuğuna tıklayıp Email Adresi giriniz
         us_004.createHotelEmailBox.sendKeys("yucedalo@gmail.com");
+        extentTest.info("Email kutucuguna veri gonderildi");
 
     }
 
--- src/test/java/tests/us_004/TC_04.java
+++ src/test/java/tests/us_004/TC_04.java
@@ -18,43 +18,72 @@ public class TC_04 extends TestBaseRapor {
     //TC_004 "hotel was inserted successfully" yazısını görebilmeli ve "ok" butonu tıklanabilir olmalı
     @Test
     public void test04() throws InterruptedException {
-        extentTest=extentReports.createTest("kullanıcı hesabı olusturma islemi testi",
-                "kullanıcı hesap olusturma islemlerini yapmasi gerekir");
+        extentTest=extentReports.createTest("HotelRoom was inserted successfully\" yazısının görünürlüğü testi",
+                "HotelRoom was inserted successfully\" yazısının görünürlüğü test edilmeli ");
+
         //1- Yonetici url e gider.
         Driver.getDriver().get(ConfigReader.getProperty("HMCUrl"));
+        extentTest.info("Url gidildi");
+
         //2- Login butonunu tiklar.
         hmcPages.firstLogInElement.click();
+        extentTest.info("Log in butonuna tiklandi");
+
         //3- Yonetici kendisine verilen username ve password ile giris yapar.
         hmcPages.userNameBoxElement.sendKeys(ConfigReader.getProperty("HMCValidUsername"));
         hmcPages.passwordBoxElement.sendKeys(ConfigReader.getProperty("HMCValidPassword"));
         hmcPages.secondLogInElement.click();
+        extentTest.info("Gecerli username ve password ile giris yapildi");
+
         //4- "Hotel management" kismina tiklar.
         hmcPages.hotelManagementLink.click();
+        extentTest.info("Hotel management linkine tiklandi");
+
         //5- Gorunur hale gelen "Hotel List" kismina tiklar.
         hmcPages.hotelListLinki.click();
-        //7- "Add Hotel" butonuna erisebilir.
+        extentTest.info("Hotel List linkine tiklandi");
+
+        //6- "Add Hotel" butonuna erisebilir.
         Assert.assertTrue(hmcPages.addHotelLinki.isDisplayed());
-        hmcPages.addHotelLinki.click();
+        extentTest.info("Add hotel kisminin gorunur oldugu goruldu");
+
         //7-"Code" kutucuğuna tıklayıp bir Kod giriniz
         hmcPages.addHotelCodeBox.sendKeys("187150015");
+        extentTest.info("Code kutucuguna veri gonderildi");
+
         //8-"Name" kutucuğuna tıklayıp bir İsim giriniz
         us_004.createHotelNameBox.sendKeys("Oguzhan");
+        extentTest.info("Name kutucuguna veri gonderildi");
+
         //9-"Adress" kutucuğuna tıklayıp Adres giriniz
         us_004.createHotelAddressBox.sendKeys("13313 cutten rd #3201");
+        extentTest.info("Adress kutucuguna veri gonderildi");
+
         //10-"Phone" kutucuğuna tıklayıp Telefon Numarası giriniz
         us_004.createHotelPhoneBox.sendKeys("8325604012");
+        extentTest.info("Phone kutucuguna veri gonderildi");
+
         //11-"Mail" kutucuğuna tıklayıp Email Adresi giriniz
         us_004.createHotelEmailBox.sendKeys("yucedalo@gmail.com");
+        extentTest.info("Email kutucuguna veri gonderildi");
+
+        us_004.createHotelEmailBox.sendKeys("yucedalo@gmail.com");
         //12-"IDGroup" dropdown butonuna tıklayıp Grup seçiniz
+        extentTest.info("Dropdown secenegi secildi");
         us_004.createHotelDropdownButton();
+
         //13-"Save" butonuna tıklayın
-        Thread.sleep(5000);
         us_004.createHotelSaveBox.click();
+        extentTest.info("Save secenegi secildi");
+
         //14-"HotelRoom was inserted successfully" yazısının görünürlüğü test edilmeli
         Thread.sleep(5000);
         Assert.assertTrue(us_004.createHotelWasInsertedSuccessfullyText.isDisplayed());
+        extentTest.info("HotelRoom was inserted successfully goruldugu test edildi");
+
         //15-"OK" butonuna tıklayın ve otel listesini tıklayın
         us_004.createHotelWasInsertedSuccessfullyOkButton.click();
+        extentTest.info("Ok butonuna tiklandi");
 
 
 
